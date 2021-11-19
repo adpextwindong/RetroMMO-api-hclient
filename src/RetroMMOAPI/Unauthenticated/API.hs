effe1ffa7395f3d24605decc141d44757086418e
Takumi Crary <tcrary@gmail.com>
2021-11-19 12:16:12 +0900

    Add TODO regarding last bit of work towards getUser

--- src/RetroMMOAPI/Unauthenticated/API.hs
+++ src/RetroMMOAPI/Unauthenticated/API.hs
@@ -11,6 +11,7 @@ import Servant.Client
 import RetroMMOAPI.Request (RMGet, RMRequest)
 import RetroMMOAPI.Types (Username, UserDetails)
 
+--TODO write File extension Ext combinator using axmason6 & basvandijk's ideas
 type User = "users"
           :> Capture "user" Username
           :> RMGet UserDetails
